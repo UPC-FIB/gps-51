e189985b0ae7d7af78d329668b0f01fd391147b6
Mikigb77 <miquelgarcia.alu@gmail.com>
2023-12-31 13:14:18 +0100

    update

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
     print("Quina transformació vols?")
     print("[1] Text amb tot majuscules")
     print("[2] Text amb tot minúscuies")
-    print("[3] Text capitalitzat ")
+    print("[3] Text capitalitzat")
 
     opcio = input("opció escollida: ")
 
